a82ae62410d5bbcd28dd7fddc1e4b8976b2a0421
Mike Evans <bundance@users.noreply.github.com>
2017-08-14 18:55:32 +0100

    create initial rudimentary UI

---
+++ src/components/app/app.component.js
@@ -0,0 +1,99 @@
+import React, { Component } from 'react';
+import logo from '../../logo.svg';
+import './app.css';
+import { Button, Col, ControlLabel, Form, FormControl, FormGroup, Grid, Row } from 'react-bootstrap';
+import DatePicker from 'react-bootstrap-date-picker';
+
+class App extends Component {
+    render() {
+        return (
+            <Form inline>
+                <Grid className="App">
+                    <Row className="App-header">
+                        <img src={logo} className="App-logo" alt="logo" />
+                        <h2>Coin History</h2>
+                    </Row>
+                    <Row>
+                        <h2>
+                            Data
+                        </h2>
+                    </Row>
+                    <Row>
+                        <FormGroup controlId="coin">
+                            <ControlLabel>Coin: </ControlLabel>
+                            <FormControl componentClass="select" placeholder="select">
+                                <option value="bitcoin">Bitcoin</option>
+                                <option value="Ethereum">Ethereum</option>
+                                <option value="LiteCoin">LiteCoin</option>
+                            </FormControl>
+                        </FormGroup>
+                    </Row>
+                    <Row>
+                        <FormGroup controlId="fields">
+                            <ControlLabel>Fields: </ControlLabel>
+                        </FormGroup>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Time</th>
+                                    <th>Low</th>
+                                    <th>High</th>
+                                    <th>Open</th>
+                                    <th>Close</th>
+                                    <th>Volume</th>
+                                </tr>
+                            </thead>
+                        </table>
+                    </Row>
+                    <Row>
+                        <FormGroup controlId="dateFormat">
+                            <ControlLabel>Format Date and Time as: </ControlLabel>
+                            <FormControl componentClass="select" placeholder="select">
+                                <option value="ddmmyy">dd-mm-yy</option>
+                                <option value="mmddyy">mm-dd-yy</option>
+                                <option value="custom">custom format</option>
+                            </FormControl>
+                        </FormGroup>
+                    </Row>
+                    <Row>
+                        <h2>Period</h2>
+                        <Col md={4}>
+                            <ControlLabel>From: </ControlLabel>
+                            <DatePicker id="from"  />
+                        </Col>
+                        <Col md={4}>
+                            <ControlLabel>To: </ControlLabel>
+                            <DatePicker id="to" />
+                        </Col>
+                        <Col md={4}>
+                            <FormGroup controlId="granularity">
+                                <ControlLabel>Granularity: </ControlLabel>
+                                <FormControl
+                                    type="text"
+                                    value={'some text'}
+                                    placeholder="Enter seconds"
+                                />
+                            </FormGroup>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <h2>Files</h2>
+                        <FormGroup controlId="splitFiles">
+                            <ControlLabel>Number of files: </ControlLabel>
+                            <FormControl
+                                type="text"
+                                value={'num of files'}
+                                placeholder="Enter files"
+                            />
+                        </FormGroup>
+                    </Row>
+                    <Row>
+                        <Button>Download</Button>
+                    </Row>
+                </Grid>
+            </Form>
+        );
+    }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import registerServiceWorker from './registerServiceWorker';
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import createSagaMiddleware from "redux-saga";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap-theme.css";
+import reducer from "./state/index";
+import App from "./components/app/app.component";
+import initialState from "./state/initial-state";
+import sagas from "./state/sagas-registration";
+const sagaMiddleware = createSagaMiddleware();
+
+let store = createStore(
+    reducer,
+    initialState,
+    applyMiddleware(sagaMiddleware)
+);
+
+sagaMiddleware.run(sagas);
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById("root")
+);
+
+registerServiceWorker();
