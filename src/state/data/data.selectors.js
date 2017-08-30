051270a9d06cac634586ae13afd420aabde5e984
Mike Evans <bundance@users.noreply.github.com>
2017-08-30 17:05:44 +0100

    add data.selectors unit tests

--- src/state/data/data.selectors.js
+++ src/state/data/data.selectors.js
@@ -2,6 +2,7 @@ import { createSelector } from 'reselect';
 import * as R from 'ramda';
 import * as dataStoreKeys from '../../constants/store-keys/data-store-keys';
 import Papa from 'papaparse';
+import trace from '../../dev/trace';
 
 export const selectHistoricPricesSample = R.path([dataStoreKeys.DATA, dataStoreKeys.HISTORIC_PRICES_SAMPLE]);
 export const selectFromDate = R.path([dataStoreKeys.DATA, dataStoreKeys.FROM_DATE]);
@@ -13,25 +14,50 @@ export const getFormValues = createSelector(
     mapUIApiNameToMarketApiName
 );
 
+/**
+ * getHistoricPricesSample object - pass in the state object, and it returns an
+ * array of formatted objects, converted from raw CSV.
+ *
+ * selectHistoricPricesSample() will return raw csv from the store like this:
+ *  `1503920460,1,2,3,4,5
+ *   1503920400,11,12,13,14,15`;
+ *
+ *  The compose function then converts this to an array of formatted objects, like this:
+ *   [{
+        date: 1503920460,
+        open: 1,
+        close: 2,
+        high: 3,
+        low: 4,
+        volume: 5
+    }, {
+        date: 1503920400,
+        open: 11,
+        close: 12,
+        high: 13,
+        low: 14,
+        volume: 15
+    }];
+ *
+ */
 export const getHistoricPricesSample = createSelector(
     [selectHistoricPricesSample],
-    prices => {
-        const csvArray = Papa.parse(prices);
-        const csvArrayOfObjs = R.map(data => R.zipObj(['date', 'open', 'close', 'high', 'low', 'volume'], data), csvArray.data);
-        console.log({ prices, csvArray, csvArrayOfObjs });
-        return csvArrayOfObjs;
-    }
+    R.compose(
+        R.map(R.zipObj(['date', 'open', 'close', 'high', 'low', 'volume'])),
+        R.path(['data']),
+        R.partialRight(Papa.parse, [{ dynamicTyping: true }])
+    )
 );
 
-export const getFirstTenPrices = createSelector(
+export const getFirstXPrices = R.curry((x,state) => createSelector(
     [getHistoricPricesSample],
-    R.take(10)
-);
+    R.take(x)
+)(state));
 
-export const getLastTenPrices = createSelector(
+export const getLastXPrices = R.curry((x, state) => createSelector(
     [getHistoricPricesSample],
-    R.takeLast(10)
-);
+    R.takeLast(x)
+)(state));
 
 
 ////// HELPERS //////
--- src/state/data/data.selectors.spec.js
+++ src/state/data/data.selectors.spec.js
@@ -2,29 +2,191 @@ import * as selectors from './data.selectors';
 import * as dataStoreKeys from '../../constants/store-keys/data-store-keys';
 
 describe('data selectors tests', () => {
-    describe('getHistoricPricesSample()', () => {
-        const mockPricesCSV = `1503920460,4319.98,4319.98,4319.98,4319.98,0.0000046\\n
-        1503920400,4318.24,4319.98,4319.98,4319.98,1.4392226700000001\\n
-        1503920340,4318.24,4319.98,4319.98,4319.98,0.4933771500000001\\n
-        1503920280,4318.67,4319.99,4318.67,4319.98,8.34976003\\n
-        1503920220,4318.64,4318.67,4318.66,4318.67,0.9104138000000002\\n
-        1503920160,4317,4318.68,4317,4318.67,1.0130253599999999\\n
-        1503920100,4316.99,4318.6,4318.59,4317,0.67826558\\n
-        1503920040,4316.99,4318.63,4316.99,4318.6,6.217752910000001\\n
-        1503919980,4315.32,4316.99,4316.99,4316.99,1.4957775599999998\\n
-        1503919920,4316.98,4316.99,4316.99,4316.99,1.4317222199999995`;
+    describe('getFirstXPrices()', () => {
+        let mockPricesCSV;
+        let mockState;
 
-        const mockState = {
-            [dataStoreKeys.DATA]: {
-                [dataStoreKeys.HISTORIC_PRICES_SAMPLE]: mockPricesCSV
-            }
-        };
+        beforeEach(() => {
+            mockPricesCSV = `1503920460,1,2,3,4,5
+                1503920401,11,12,13,14,15
+                1503920402,21,22,23,24,25
+                1503920403,31,32,33,34,35
+                1503920404,41,42,43,44,45`;
+
+            mockState = {
+                [dataStoreKeys.DATA]: {
+                    [dataStoreKeys.HISTORIC_PRICES_SAMPLE]: mockPricesCSV
+                }
+            };
+        });
+
+        it('should return the first 2 prices', () => {
+            const actual = selectors.getFirstXPrices(2, mockState);
+
+            const expected = [{
+                date: 1503920460,
+                open: 1,
+                close: 2,
+                high: 3,
+                low: 4,
+                volume: 5
+            }, {
+                date: 1503920401,
+                open: 11,
+                close: 12,
+                high: 13,
+                low: 14,
+                volume: 15
+            }];
+
+            expect(actual).toEqual(expected);
+        });
+
+        it('should return the first 3 prices', () => {
+            const actual = selectors.getFirstXPrices(3, mockState);
+
+            const expected = [{
+                date: 1503920460,
+                open: 1,
+                close: 2,
+                high: 3,
+                low: 4,
+                volume: 5
+            }, {
+                date: 1503920401,
+                open: 11,
+                close: 12,
+                high: 13,
+                low: 14,
+                volume: 15
+            }, {
+                date: 1503920402,
+                open: 21,
+                close: 22,
+                high: 23,
+                low: 24,
+                volume: 25
+            }];
+
+            expect(actual).toEqual(expected);
+        });
+    });
+
+    describe('getFirstXPrices()', () => {
+        let mockPricesCSV;
+        let mockState;
+
+        beforeEach(() => {
+            mockPricesCSV = `1503920460,1,2,3,4,5
+                1503920401,11,12,13,14,15
+                1503920402,21,22,23,24,25
+                1503920403,31,32,33,34,35
+                1503920404,41,42,43,44,45`;
+
+            mockState = {
+                [dataStoreKeys.DATA]: {
+                    [dataStoreKeys.HISTORIC_PRICES_SAMPLE]: mockPricesCSV
+                }
+            };
+        });
+
+        it('should return the last 2 prices', () => {
+            const actual = selectors.getLastXPrices(2, mockState);
+
+            const expected = [{
+                date: 1503920403,
+                open: 31,
+                close: 32,
+                high: 33,
+                low: 34,
+                volume: 35
+            }, {
+                date: 1503920404,
+                open: 41,
+                close: 42,
+                high: 43,
+                low: 44,
+                volume: 45
+            }];
+
+            expect(actual).toEqual(expected);
+        });
+
+        it('should return the last 3 prices', () => {
+            const actual = selectors.getLastXPrices(3, mockState);
+
+            const expected = [{
+                date: 1503920402,
+                open: 21,
+                close: 22,
+                high: 23,
+                low: 24,
+                volume: 25
+            }, {
+                date: 1503920403,
+                open: 31,
+                close: 32,
+                high: 33,
+                low: 34,
+                volume: 35
+            }, {
+                date: 1503920404,
+                open: 41,
+                close: 42,
+                high: 43,
+                low: 44,
+                volume: 45
+            }];
+
+            expect(actual).toEqual(expected);
+        });
+    });
+
+    describe('getHistoricPricesSample()', () => {
+        let mockPricesCSV;
+        let mockState;
+
+        beforeEach(() => {
+            mockPricesCSV = `1503920460,1,2,3,4,5
+                1503920400,11,12,13,14,15`;
+
+            mockState = {
+                [dataStoreKeys.DATA]: {
+                    [dataStoreKeys.HISTORIC_PRICES_SAMPLE]: mockPricesCSV
+                }
+            };
+        });
+
 
         it('should convert the historic prices csv string into an array of objects', () => {
             const actual = selectors.getHistoricPricesSample(mockState);
 
             expect(Array.isArray(actual)).toEqual(true);
-            expect(typeof actual[0] === 'Object').toEqual(true);
+            expect(typeof actual[0] === 'object').toEqual(true);
         });
+
+        it('should add property names to the raw data', () => {
+            const actual = selectors.getHistoricPricesSample(mockState);
+
+            const expected = [{
+                date: 1503920460,
+                open: 1,
+                close: 2,
+                high: 3,
+                low: 4,
+                volume: 5
+            }, {
+                date: 1503920400,
+                open: 11,
+                close: 12,
+                high: 13,
+                low: 14,
+                volume: 15
+            }];
+
+            expect(actual).toEqual(expected);
+        })
     });
+
+
 });
