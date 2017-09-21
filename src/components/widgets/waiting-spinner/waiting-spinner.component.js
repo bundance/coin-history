ba638181423cc148ed130da1faede411536e921d
Mike Evans <bundance@users.noreply.github.com>
2017-09-21 22:55:53 +0100

    tidy up

--- src/components/widgets/waiting-spinner/waiting-spinner.component.js
+++ src/components/widgets/waiting-spinner/waiting-spinner.component.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
 import Spinner from "react-md-spinner";
 import MediaQuery from 'react-responsive';
 import { Modal } from 'react-bootstrap';
