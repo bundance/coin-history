f8e6ccb46956427067bc9a286c65cce69a50e5e0
Mike Evans <bundance@users.noreply.github.com>
2017-09-24 13:45:37 +0100

    add text to the About page

--- src/components/pages/about/about.component.js
+++ src/components/pages/about/about.component.js
@@ -3,7 +3,17 @@ import {Grid, Row } from 'react-bootstrap';
 
 const About = () =>  (
     <Grid className="App">
-        <Row>About</Row>
+        <h2>About</h2>
+        <p>
+            CoinHistory is a simple client interface to the GDAX <a href="https://docs.gdax.com/#get-historic-rates">
+            getHistoricRates</a> and <a href="https://docs.gdax.com/#get-products">getProducts</a> endpoints.
+        </p>
+        <p>
+            Created by Mike Evans using the three Rs: React, Redux and Ramda.
+        </p>
+        <p>
+            All code is open source, and can be found <a href="https://github.com/bundance/coin-history">here</a>
+        </p>
     </Grid>
 );
 
